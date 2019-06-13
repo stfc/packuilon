7e9aab14b40db861417fb7260fb9fe3fd5087d7a
Matthew Richards <matthew.richards@stfc.ac.uk>
2019-06-13 13:25:15 +0000

    Improve general logic of script by only executing OpenStack commands when required
    - This will prevent commands from failing because they don't have valid arguments causing logs to look as if something has gone wrong when in fact the command should never have been executed
    - Only shared images will have members
    - Also improved the commenting and converted some Glance commands into OpenStack commands to make use of the -f functionality

--- usr/local/bin/rename-old-images.sh
+++ usr/local/bin/rename-old-images.sh
@@ -4,8 +4,11 @@ set -eu
 imagename=$*
 DATE=`date +%Y-%m-%d`
 
-#list all the images that have the same name as the one just added
-imagesid=$(glance image-list --sort created_at:desc | grep " $imagename " | cut -d '|' -f 2)
+# List all the images that have the same name as the one just added
+echo $imagename
+imagesid=$(openstack image list --sort created_at:desc -f value | grep " $imagename " | awk '{print $1}')
+echo $imagesid
+
 if [ -z "$imagesid" ]
 then
     echo "Image name doesn't exist in the project, no work to do for rename-old-images.sh to do"
@@ -14,37 +17,47 @@ else
     newimageid=$(echo "$imagesid" | head -n 1)
     echo $newimageid
 
-    #for each image that is not the newest image with this name
+    # For each image that is not the newest image with this name
     members=''
     for id in $(echo "$imagesid" | tail -n +2)
     do
         echo $id
-        #rename
-        glance image-update --name "archive-$imagename $DATE" $id
+        # Rename image to be at bottom of the list
+        openstack image set $id --name "warehoused-$imagename $DATE"
 
-        #get list of members from old image
-        for member in $(glance member-list --image-id $id | grep $id | cut -d '|' -f 3);
-        do
-            #only add if not already in list
-            if [[ ${members} != *"$member"* ]]
-            then
-         	members="$members $member"
-            fi
-            #remove members from old image
-            glance member-delete $id $member
-        done
-    done
+        # Ensure visibility of image is 'shared' before attempting to manipulate members
+        imagevisibility=$(openstack image show "$id" -f json | jq -r .visibility)
 
-    newimagemembers=$(glance member-list --image-id $newimageid | grep $newimageid | cut -d '|' -f 3)
-
-    # add memberships to new image
-    for member in $members;
-    do
-        #don't create member if it's already there
-        if [[ ${newimagemembers} != *"$member"* ]]
+        if [ "$imagevisibility" = "shared" ]
         then
-            glance member-create $newimageid $member
+            # Get list of members from old image
+            for member in $(glance member-list --image-id $id | grep $id | cut -d '|' -f 3);
+            do
+                # Only add if not already in list
+                if [[ ${members} != *"$member"* ]]
+                then
+                    members="$members $member"
+                fi
+                # Remove members from old image
+                glance member-delete $id $member
+            done
         fi
-        glance member-update $newimageid $member accepted	
     done
+
+    if [ "$imagevisibility" = "shared" ]
+    then
+        newimagemembers=$(glance member-list --image-id $newimageid | grep $newimageid | cut -d '|' -f 3)
+ 
+        # Add memberships to new image
+        for member in $members;
+        do
+            # Don't create member if it's already there
+            if [[ ${newimagemembers} != *"$member"* ]]
+            then
+                glance member-create $newimageid $member
+            fi
+            glance member-update $newimageid $member accepted
+        done
+    fi
 fi
+
